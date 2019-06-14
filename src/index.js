3364d86ead58aa9983349ab9e79d8dd77aa89b8e
Alex <nikitin2009@gmail.com>
2019-06-14 13:55:44 +0300

    Write HTML structure for a project view

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+
+const App = (function() {
+  const entryPoint = document.getElementById('app');
+
+  function init() {
+    entryPoint.innerHTML = '<h1>Hello world</h1>';
+  }
+
+  return {
+    init,
+  }
+})();
+
+App.init();
